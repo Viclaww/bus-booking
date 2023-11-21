9dbe014b24d4b42a10a153495088fea460fc0ab4
Your Name <youremail@example.com>
2023-11-21 18:54:32 +0100

    Fixed erroe for User Dashboard

--- src/screens/UserBooking.jsx
+++ src/screens/UserBooking.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Booker from "../components/booker";
+import Booker from "../components/Booker";
 import AvailableTransits from "../components/AvailableTransits";
 
 const UserBooking = () => {
