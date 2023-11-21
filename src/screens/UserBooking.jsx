15e3f8c399c14970a653c8b005666dd8a14a4e51
Your Name <youremail@example.com>
2023-11-21 18:50:52 +0100

    Added Booking Feature

--- src/screens/UserBooking.jsx
+++ src/screens/UserBooking.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Booker from "../components/booker";
-import AvailableTransits from "../components/availableTransits";
+import AvailableTransits from "../components/AvailableTransits";
 
 const UserBooking = () => {
   return (
